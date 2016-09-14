218bedf673c612ac58492546953fc87f5dbcd772
VhatAmI <heavenyale@gmail.com>
2016-09-13 22:04:02 -0500

    Client Server chat in Go
    JS and Go integration // requires go get github.com/gorilla/websocket to
    be ran locally in order to compile

--- Workspace/chat/main.go
+++ Workspace/chat/main.go
@@ -22,11 +22,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }	
 	
 func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"}) 
-
-//start web server here
-if err := http.ListenAndServe(":8080", nil); err != nil {
-	log.Fatal("ListenAndServe: ", err)
+	go r.run()
+	//start web server here
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }	
