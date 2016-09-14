9be8bb1f1a6bfbd023a35b59ad41809a88e5ea74
VhatAmI <heavenyale@gmail.com>
2016-09-13 20:38:54 -0500

    HTML removed from go file and placed in template

--- Workspace/chat/main.go
+++ Workspace/chat/main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+type templateHandler struct {
+	once 	 sync.Once
+	filename string
+	templ 	 *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		})
+		t.templ.Execute(w,nil)
+}	
 	
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`
-		<html>
-			<head>
-				<title> Go Chat</title>
-			</head>
-			<body>
-				Lets chat!
-			</body>
-		</html>
-	`))
-})
+	http.Handle("/", &templateHandler{filename: "chat.html"}) 
+
 //start web server here
 if err := http.ListenAndServe(":8080", nil); err != nil {
 	log.Fatal("ListenAndServe: ", err)
-}
+	}
 }	
