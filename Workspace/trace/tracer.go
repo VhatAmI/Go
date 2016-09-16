c2e3282da178a13af8b1cc1d9fd15bcea4a0db83
VhatAmI <heavenyale@gmail.com>
2016-09-15 21:50:17 -0500

    Testing Framework
    go test -cover checks unit test coverage and displays which tests pass
    or fail

--- Workspace/trace/tracer.go
+++ Workspace/trace/tracer.go
@@ -1,7 +1,20 @@
 package trace
 
+import(
+	"io"
+	"fmt"
+)
 //Interface for tracing code events
 
 type Tracer interface {
 	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace (a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
--- Workspace/trace/tracer_test.go
+++ Workspace/trace/tracer_test.go
@@ -2,7 +2,23 @@ package trace
 
 import (
 	"testing"
+	"bytes"
+	"io"
 )
 func TestNew(t *testing.T) {
-	t.Error("We haven't written any tests yet")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	} else {
+	tracer.Trace("Hello trace package.")
+	if buf.String() != "Hello trace package.\n" {
+		t.Errorf("Trace should not write '%s'.", buf.String())
+	}
+	}
 }
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
