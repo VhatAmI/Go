bed113f7ad81b7a23781c0e334921940e4c3574d
VhatAmI <heavenyale@gmail.com>
2016-09-17 14:46:38 -0500

    Messages now have senders name
    Name pulled from OAuth cookie, unsure if able to use off of localhost

--- Workspace/chat/client.go
+++ Workspace/chat/client.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"time"
 )
 
 type client struct {
 	socket *websocket.Conn	
-	send chan []byte 	
+	send chan *message	
 	room *room
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
+	defer c.socket.Close()
 	for {
-	if _, msg, err := c.socket.ReadMessage(); err == nil {
-		c.room.forward <- msg
-	} else {
-	break
+		var msg *message
+	    err := c.socket.ReadJSON(&msg)
+		if err != nil { 
+			return
 		}
-	}
-	c.socket.Close()
+		msg.When = time.Now()
+		msg.Name = c.userData["name"].(string)
+		c.room.forward <- msg	
+	}	
 }
 
 func (c *client) write() {
+	defer c.socket.Close()
 	for msg := range c.send {
-	if err := c.socket.WriteMessage(websocket.TextMessage, msg);
-	err != nil {
-	break
+		err := c.socket.WriteJSON(msg)
+	if err != nil {
+		return
 	}
 }
-c.socket.Close()
+
 }
--- Workspace/chat/room.go
+++ Workspace/chat/room.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
 	"net/http"
 	"log"
 	"trace"
 )
 
 type room struct {
-	forward chan []byte
+	forward chan *message
 	
 	join chan *client
 	
@@ -31,7 +32,7 @@ func (r *room) run() {
 		close(client.send)
 		r.tracer.Trace("Client left")
 	case msg := <-r.forward:
-		r.tracer.Trace("Message recieved : ", string(msg))
+		r.tracer.Trace("Message recieved : ", msg.Message)
 		for client := range r.clients {
 		select {
 		case client.send <- msg:
@@ -60,10 +61,18 @@ if err != nil {
 	log.Fatal("ServeHTTP : ", err)
 	return
 }
+
+authCookie, err := req.Cookie("auth")
+if err != nil {
+	log.Fatal("Failed to get required authentication cookie: ", err)
+	return
+}
+
 client := &client{
 	socket: socket,
-	send: make(chan []byte, messageBufferSize),
+	send: make(chan *message, messageBufferSize),
 	room: r,
+	userData: objx.MustFromBase64(authCookie.Value),
 }
 r.join <- client
 defer func() { r.leave <-client }()
@@ -73,7 +82,7 @@ client.read()
 
 func newRoom() *room {
 	return &room{
-	forward: make(chan []byte),
+	forward: make(chan *message),
 	join:    make(chan *client),
 	leave:   make(chan *client),
 	clients: make(map[*client]bool),
