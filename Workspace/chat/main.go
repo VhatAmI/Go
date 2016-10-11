d371aa7500175a2d9bc469862bde81305ef909c6
Matt <heavenyale@gmail.com>
2016-10-11 17:09:47 -0500

    Log out clears cookie value, removes cookie entirely with some browsers

--- Workspace/chat/main.go
+++ Workspace/chat/main.go
@@ -54,6 +54,18 @@ func main() {
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name: "auth",
+		Value: "",
+		Path: "/",
+		MaxAge: -1,
+
+	})
+	w.Header()["Location"] = []string{"/chat"}
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
 	go r.run()
 	//start web server here
 	log.Println("Starting server on ", *addr)
